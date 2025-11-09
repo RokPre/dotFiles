8dfdc7046dee7a2e6da527e893b839fba63163ea
Rok Prezelj <prezelj.rok@gmail.com>
2025-11-09 10:55:53 +0100

    Some changes

--- i3/scripts/brightness.sh
+++ i3/scripts/brightness.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-step=1000
-backlight="/sys/class/backlight/intel_backlight"
-curr=$(cat "$backlight/brightness")
-max=$(cat "$backlight/max_brightness")
-
 if [ "$1" = "up" ]; then
-    new=$((curr + step))
-    echo up
+    /usr/bin/brightnessctl set +5%
 elif [ "$1" = "down" ]; then
-    new=$((curr - step))
-    echo down
-else
-    exit 1
+    /usr/bin/brightnessctl set 5%-
 fi
-
-echo "$new" | sudo tee "$backlight/brightness" > /dev/null
