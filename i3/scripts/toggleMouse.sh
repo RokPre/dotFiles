c1350012e2571a73b45824ae736c1fbacdc6a25e
RokPre <prezelj.rok@gmail.com>
2026-01-16 16:20:11 +0100

    Added natural scrolling to laptop trackpad, just in case

--- i3/scripts/toggleMouse.sh
+++ i3/scripts/toggleMouse.sh
@@ -5,6 +5,8 @@ enabled=$(xinput list-props "ELAN06FA:00 04F3:327E Touchpad" \
   | awk -F: '{print $2}' \
   | tr -d '[:space:]')
 
+xinput set-prop "ELAN06FA:00 04F3:327E Touchpad" "libinput Natural Scrolling Enabled" 1
+
 if [[ "$enabled" == "1" ]]; then
   xinput disable "ELAN06FA:00 04F3:327E Touchpad"
   xdotool mousemove 0 0
