7fe8c02540e337b720a6caf58c16b8a7c6c52885
Rok Prezelj <prezelj.rok@gmail.com>
2026-01-27 22:46:25 +0100

    Conform now formats on save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,45 +1,50 @@
 return {
-  "stevearc/conform.nvim",
-  config = function()
-    local ok, mason = pcall(require, "mason")
-    if ok then
-      local cfg = require("mason.settings").current
-      local ensure = cfg.ensure_installed or {}
+	"stevearc/conform.nvim",
+	config = function()
+		local ok, mason = pcall(require, "mason")
+		if ok then
+			local cfg = require("mason.settings").current
+			local ensure = cfg.ensure_installed or {}
 
-      vim.list_extend(ensure, {
-        "stylua",
-        "autopep8",
-        "autoflake",
-        "reorder-python-imports",
-        "xmlformatter",
-        "shfmt"
-      })
+			vim.list_extend(ensure, {
+				"stylua",
+				"autopep8",
+				"autoflake",
+				"reorder-python-imports",
+				"xmlformatter",
+				"shfmt",
+			})
 
-      cfg.ensure_installed = ensure
+			cfg.ensure_installed = ensure
 
-      local mr_ok, mr = pcall(require,"mason-registry")
-      if mr_ok then
-        vim.defer_fn(function()
-          mr.refresh(function()
-            for _, tool in ipairs(cfg.ensure_installed) do
-              local p = mr.get_package(tool)
-              if not p:is_installed() then
-                p:install()
-              end
-            end
-          end)
-        end, 100)
-      end
-    end
+			local mr_ok, mr = pcall(require, "mason-registry")
+			if mr_ok then
+				vim.defer_fn(function()
+					mr.refresh(function()
+						for _, tool in ipairs(cfg.ensure_installed) do
+							local p = mr.get_package(tool)
+							if not p:is_installed() then
+								p:install()
+							end
+						end
+					end)
+				end, 100)
+			end
+		end
 
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "autopep8", "autoflake", "reorder-python-imports" },
-        xml = { "xmlformatter" },
-        bash = {"shfmt"},
-        ["_"] = { "trim_whitespace" },
-      },
-    })
-  end,
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "autopep8", "autoflake", "reorder-python-imports" },
+				xml = { "xmlformatter" },
+				bash = { "shfmt" },
+				["*"] = { "trim_whitespace" },
+			},
+			format_on_save = {
+				-- I recommend these options. See :help conform.format for details.
+				lsp_format = "fallback",
+				timeout_ms = 500,
+			},
+		})
+	end,
 }
