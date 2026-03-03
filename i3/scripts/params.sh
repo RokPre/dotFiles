fb5b064620adc8c13646bb0773864a2f3d91cfd4
Rok Prezelj <prezelj.rok@gmail.com>
2026-03-03 16:29:30 +0100

    Params are now stored in the /tmp and the script is executed inside of the main config.

--- i3/scripts/params.sh
+++ i3/scripts/params.sh
@@ -2,16 +2,13 @@
 
 . /etc/os-release
 
-params="$HOME/.config/i3/params"
+params="/tmp/params"
 
 browser_cmd="$(command -v qutebrowser 2>/dev/null || command -v vivaldi 2>/dev/null || true)"
 
 [ -n "$browser_cmd" ] || { echo "qutebrowser or vivaldi not found in PATH" >&2; exit 1; }
 
 cat > "$params" <<EOF
-# Look at scripts/params.sh
-exec_always --no-startup-id ~/.config/i3/scripts/params.sh
-
 set \$browser_cmd $browser_cmd
 
 bindsym \$mod+Ctrl+i exec --no-startup-id \$browser_cmd; workspace \$ws2
