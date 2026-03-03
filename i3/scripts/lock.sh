75da1fb8e3d7f1bb94240ef052d5e4cc352a51d7
Rok Prezelj <prezelj.rok@gmail.com>
2026-03-03 16:52:36 +0100

    Added mute and unmute and added pause and play.

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+# Mute
+amixer set Master mute > /dev/null
+# Pause
+playerctl pause
+
 # Capture screenshot
 scrot -o /tmp/screenshot.png
 # Blur screenshot
-magick /tmp/screenshot.png -blur 0x5 /tmp/screenshot.png
-# Mute
-amixer set Master mute > /dev/null
+$(command -v magick || command -v convert) /tmp/screenshot.png -blur 0x5 /tmp/screenshot.png
 # Lock
-i3lock -i /tmp/screenshot.png
+i3lock -n -i /tmp/screenshot.png
+
+# Unmute
+amixer set Master unmute > /dev/null
+# Resume
+playerctl play
