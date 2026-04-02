1bd5603598d951bab2c34ae6ee7eb41ed33a0689
Rok Prezelj <prezelj.rok@gmail.com>
2026-04-02 13:51:56 +0200

    Made markdown preview follow theme

--- nvim/lua/plugins/markdownPreview.lua
+++ nvim/lua/plugins/markdownPreview.lua
@@ -6,6 +6,8 @@ return {
 		vim.g.mkdp_filetypes = { "markdown" }
 		vim.g.mkdp_images_path = "/home/rok/sync/knowledgeVault"
 		vim.g.mkdp_markdown_css = "/home/rok/sync/dotFiles/nvim/lua/plugins/markdownPreview.css"
+		vim.g.mkdp_auto_close = 0
+		vim.g.mkdp_combine_preview = 0
 	end,
 	ft = { "markdown" },
 }
